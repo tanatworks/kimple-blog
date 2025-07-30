718c6ecdf4d3c94a2ce4890fac226122c2eafcdf
Tanat <tanat.ker@dome.tu.ac.th>
2025-07-30 17:08:15 +0700

    feat: Add login functionality and create login page

---
+++ public/login.js
@@ -0,0 +1,14 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+	e.preventDefault();
+	const username = document.getElementById("username").value.trim();
+	const password = document.getElementById("password").value;
+
+	const validUsername = "admin";
+	const validPassword = "1234";
+
+	if (username === validUsername && password === validPassword) {
+		window.location.href = "admin.html";
+	} else {
+		alert("Invalid username or password");
+	}
+});
